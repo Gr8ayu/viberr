8736110f8addadf95f99219210cad47b132770c6
Ayush Kumar <anonymouskmr@gmail.com>
2018-08-04 22:45:38 +0530

    Add files via upload

---
+++ myblog3/music/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+    # /music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+    # music/12/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name="detail"),
+
+    # music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add')
+]
